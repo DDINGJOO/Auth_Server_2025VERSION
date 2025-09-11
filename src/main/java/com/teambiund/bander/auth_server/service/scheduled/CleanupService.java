21fea78244544d49a3a5b52fbbbfce9c0fc20e64
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-11 16:06:09 +0900

    [FEAT] 스케줄 작업 및 Kafka 설정 추가
    src/main/java/com/teambiund/bander/auth_server/service/scheduled/CleanupService.java: 3년 이상 탈퇴 유저 데이터 삭제 스케줄 추가
    src/main/java/com/teambiund/bander/auth_server/service/scheduled/SuspendRelease.java: 정지 해제 스케줄 및 데이터 정합성 보장 위한 트랜잭션 추가
    src/main/java/com/teambiund/bander/auth_server/config/KafkaConfig.java: Kafka 설정 파일 추가
    .env: Kafka URL 기본값 변경 (localhost 기반)
    
    참고: ShedLock 사용으로 스케줄 중복 방지
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- src/main/java/com/teambiund/bander/auth_server/service/scheduled/CleanupService.java
+++ src/main/java/com/teambiund/bander/auth_server/service/scheduled/CleanupService.java
@@ -3,7 +3,10 @@ package com.teambiund.bander.auth_server.service.scheduled;
 
 import com.teambiund.bander.auth_server.repository.AuthRepository;
 import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +16,9 @@ public class CleanupService {
     private final AuthRepository authRepository;
 
 
+    @Scheduled(cron = "0 0 4 * * *", zone = "Asia/Seoul")
+    @SchedulerLock(name = "deleteWithdrawUserAfterThreeYears", lockAtMostFor = "10m", lockAtLeastFor = "1m")
+    @Transactional
     public void deleteWithdrawUserAfterThreeYears() {
         authRepository.deleteByDeletedAtBefore((LocalDateTime.now()));
     }
--- src/main/java/com/teambiund/bander/auth_server/service/scheduled/SuspendRelease.java
+++ src/main/java/com/teambiund/bander/auth_server/service/scheduled/SuspendRelease.java
@@ -5,7 +5,10 @@ import com.teambiund.bander.auth_server.enums.Status;
 import com.teambiund.bander.auth_server.repository.AuthRepository;
 import com.teambiund.bander.auth_server.repository.SuspendRepository;
 import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,6 +22,9 @@ public class SuspendRelease {
 
     //[ 04:00 시에 정지기간이 끝나는 날인 유저 정지 해제 ]
     //TODO : SOLVE N+1 ISSUE
+    @Scheduled(cron = "0 0 3 * * *", zone = "Asia/Seoul")
+    @SchedulerLock(name = "releaseUserAfter suspended Until", lockAtMostFor = "10m", lockAtLeastFor = "1m")
+    @Transactional
     public void release() {
         List<Suspend> suspends = suspendRepository.findAllBySuspendUntilIsBefore((LocalDate.now().minusDays(1)));
         authRepository.findAllById(suspends.stream().map(Suspend::getSuspendedUserId).toList())
